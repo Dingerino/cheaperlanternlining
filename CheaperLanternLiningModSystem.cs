459a507defd4ebd4d3ab9f804bec5a39020d8bde
Dingerino <frostysniper96@gmail.com>
2025-04-29 18:43:24 -0400

    Add files via upload

---
+++ CheaperLanternLiningModSystem.cs
@@ -0,0 +1,21 @@
+﻿using CheaperLanternLining.Items;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace CheaperLanternLining
+{
+    public class CheaperLanternLiningModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.RegisterItemClass(Mod.Info.ModID + ".itemlining", typeof(ItemLining));
+        }
+
+    }
+}
---
+++ Items/Itemlining.cs
@@ -0,0 +1,20 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace CheaperLanternLining.Items
+{
+    internal class ItemLining : ItemMetalPlate  //Act like a metal plate
+    {
+        public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
+        {
+            //Interact like a metal plate if the target block is a lantern
+            if (blockSel != null && byEntity.World.BlockAccessor.GetBlock(blockSel.Position) is BlockLantern)
+            {
+                base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
+                return;
+            }
+            //Do nothing otherwise
+            handling = EnumHandHandling.PreventDefault;
+        }
+    } 
+}
